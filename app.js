4df48ec85bccfd853ad805def6c9d8ee63aa7723
Mikakoyoo <shinsuengju1@gmail.com>
2022-10-28 06:48:08 +0900

    #2.6 Objects

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const dasyOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+const player = {
+    name: "Mikakoyoo",
+    points: 10,
+    fat: true,
+};
 
-// Get Item from Array
-console.log(dasyOfWeek);
+console.log(player); // player 내용 전체 표시
+console.log(player.name); // player 안에 name 만 표시
+console.log(player["name"]); // player.name 과 동일, 다른 표현 방법
 
-// Add one more day to the array
-dasyOfWeek.push("sun");
+player.fat = false; // true에서 false로 내용 업데이트
+console.log(player);
 
-console.log(dasyOfWeek);
+player.lastName = "potato"; // LastName 항목이 추가가 됨
+player.points = 15; // 점수 수정
+console.log(player);
 
-console.log(dasyOfWeek[4]);
+player.points = player.points + 15; // 현재 점수에서 더하기 처리
+console.log(player);
