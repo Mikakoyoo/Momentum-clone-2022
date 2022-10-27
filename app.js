23b30111d079b2649e44cc0a6aa56d6b01096072
Mikakoyoo <shinsuengju1@gmail.com>
2022-10-28 07:27:32 +0900

    #2.7 Functions part One

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-const player = {
-    name: "Mikakoyoo",
-    points: 10,
-    fat: true,
-};
+function sayHello(){
+    console.log("Hello my name is C");
+}
 
-console.log(player); // player 내용 전체 표시
-console.log(player.name); // player 안에 name 만 표시
-console.log(player["name"]); // player.name 과 동일, 다른 표현 방법
+console.log("hello");
+sayHello("Mikakoyoo"); // Mikakoyoo가 전혀 반영 되지 않음
 
-player.fat = false; // true에서 false로 내용 업데이트
-console.log(player);
-
-player.lastName = "potato"; // LastName 항목이 추가가 됨
-player.points = 15; // 점수 수정
-console.log(player);
-
-player.points = player.points + 15; // 현재 점수에서 더하기 처리
-console.log(player);
+console.log("hello  ")
