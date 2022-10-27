1b13ab837e79daf3dc1c09f0269da9222494cda6
Mikakoyoo <shinsuengju1@gmail.com>
2022-10-28 06:11:48 +0900

    #2.5 Arrays

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const dasyOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(dasyOfWeek);
+
+// Add one more day to the array
+dasyOfWeek.push("sun");
+
+console.log(dasyOfWeek);
+
+console.log(dasyOfWeek[4]);
