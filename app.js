67a16e819eaf987531ee20dce5f386f93fb0d6d2
Mikakoyoo <shinsuengju1@gmail.com>
2022-10-28 05:42:35 +0900

    #2.3 Const and let

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const a = 10;
 const b = 2;
-
-const myName = "Mikakoyoo"
+// var이것도 사용은 가능, 예전 자바스크립트는 다 이걸 사용
+let myName = "Mikakoyoo";
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
 console.log("Hello " + myName);
+
+myName = "Ted";
+
+console.log("your new name is " + myName);
