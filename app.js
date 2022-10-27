7f3c99bfe3d4e910e8795d709f54bce6c545aa41
Mikakoyoo <shinsuengju1@gmail.com>
2022-10-28 05:51:05 +0900

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const a = 10;
-const b = 2;
-// var이것도 사용은 가능, 예전 자바스크립트는 다 이걸 사용
-let myName = "Mikakoyoo";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("Hello " + myName);
-
-myName = "Ted";
-
-console.log("your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
