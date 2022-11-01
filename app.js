9f861c5b83f337802f03ad8d4c9ae73e7023f7e1
Mikakoyoo <shinsuengju1@gmail.com>
2022-11-01 17:21:16 +0900

    #3.3 Events

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const title = document.querySelector(".hello h1");
-// const title = document.querySelector("#hello form");
-// const title = document.querySelectorAll(".hello h1");
-// const title = document.getElementById("hello");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
+// function handTitleClick() {
+//   console.log("title was clicked!");
+// }
+
+// title.addEventListener("click", handTitleClick);
+
+function handTitleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handTitleClick);
