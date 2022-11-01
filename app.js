c967b34221a46ac67d9bbe254a828e58b3da3812
Mikakoyoo <shinsuengju1@gmail.com>
2022-11-01 13:07:05 +0900

    #2.14 Conditionals part Two

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const age = prompt("How old are you?"); // JS가 코드의 실행을 멈춤 그래서 prompt는 안씀.
-// 답을 하고 나야 아래 콘솔이 실행이 됨
+const age = parseInt(prompt("How old are you?"));
 
-console.log(age, parseInt(age));
-console.log(typeof "15", typeof parseInt("15"));
-
-
-const age2 = parseInt(prompt("How old are you?")); // JS가 코드의 실행을 멈춤 그래서 prompt는 안씀.
-// 답을 하고 나야 아래 콘솔이 실행이 됨
-
-console.log(age2);
+if (isNaN(age)) {
+  /// condition === true
+  console.log("Please write a number");
+} else {
+  /// condition === false
+  console.log("Thank you for writing your age.");
+}
