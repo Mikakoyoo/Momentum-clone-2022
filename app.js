7ada5244869138133e97a81cc4f94fa0dfaaf479
Mikakoyoo <shinsuengju1@gmail.com>
2022-11-03 11:35:28 +0900

    #3.8 CSS in JavaScript Part Three

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handTitleClick() {
+//   const clickedClass = "clicked";
+//   if (h1.classList.contains(clickedClass)) {
+//     h1.classList.remove(clickedClass);
+//   } else {
+//     h1.classList.add(clickedClass);
+//   }
+// }
+
+// h1.addEventListener("click", handTitleClick);
+//위에 코드는 toggle로 줄일 수 있음
+
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handTitleClick() {
-  const clickedClass = "clicked";
-  if (h1.className === "clickedClass") {
-    h1.className = "";
-  } else {
-    h1.className = "clickedClass";
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handTitleClick);
