b788abe55ecedca9d85b376d4880209aef3d2e73
Mikakoyoo <shinsuengju1@gmail.com>
2022-11-05 05:40:26 +0900

    #4.3 Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+<<<<<<< HEAD
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+=======
+// 위에 코드와 아래 코드는 같은 것
+// const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+
+// function onLoginBtnClick() {
+//   const username = loginInput.value;
+//   if (username === "") {
+//     alert("please write your name");
+//   } else if (username.length > 15) {
+//     alert("your name is too long.");
+//   }
+// }
+// loginButton.addEventListener("click", onLoginBtnClick);
+
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  console.log(username);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
+>>>>>>> parent of d67bb1d (#4.1 Form Submission)
