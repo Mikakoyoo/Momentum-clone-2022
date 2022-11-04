33b43a922dbc2fd4981526b0eafc13ce86de09f9
Mikakoyoo <shinsuengju1@gmail.com>
2022-11-05 06:56:15 +0900

    Update #4.4 Getting Username

--- app.js
+++ app.js
@@ -4,22 +4,12 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 
-const link = document.querySelector("a");
-
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  // greeting.innerText = "Hello " + username;
-  // 아래와 같은 것, 다른 표기 방식
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
-}
-
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
